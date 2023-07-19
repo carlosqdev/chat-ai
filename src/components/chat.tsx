7702b7a80bb788127214fb192d2eaff76191782f
carlosqdev <carlosqdev@gmail.com>
2023-07-18 21:36:35 -0600

    add styles and layout

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,32 +1,63 @@
 "use client";
 
-import { Box } from "@chakra-ui/react";
+import { useState } from "react";
 import { useChat } from "ai/react";
-import Prompt from "./prompt";
+import { Button, Flex, Grid, GridItem } from "@chakra-ui/react";
+
+import InputUser from "./inputUser";
 import ContainerMessages from "./containerMessages";
 
+import { IconExpand } from "./icons";
+
 export default function Chat() {
+  const [showSidebar, setShowSidebar] = useState(true);
+
   const { messages, input, handleInputChange, handleSubmit } = useChat({
     api: "/api/chat",
   });
 
-  return (
-    <Box as="div" border="10px solid red" width="100%" minHeight="100vh">
-      <Box
-        width="90%"
-        margin="0 auto"
-        border="10px solid blue"
-        display="grid"
-        gap="1rem"
-      >
-        <ContainerMessages messages={messages} />
+  function handleShowChatHistory() {
+    setShowSidebar(!showSidebar);
+  }
 
-        <Prompt
+  return (
+    <Grid
+      templateAreas={`"sidebar chat"
+                    "sidebar inputUser"`}
+      gridTemplateRows={"1fr 192px"}
+      gridTemplateColumns={`${showSidebar ? "260px 1fr" : "50px 1fr"}`}
+      minHeight="100vh"
+      gap="1"
+      color="blackAlpha.700"
+      fontWeight="bold"
+    >
+      <GridItem
+        pl={`${showSidebar ? "2" : "0"}`}
+        area={"sidebar"}
+        border="1px solid blue"
+      >
+        <Flex>
+          <Button
+            aria-label="Open sidebar"
+            marginLeft="auto"
+            onClick={handleShowChatHistory}
+          >
+            <IconExpand />
+          </Button>
+        </Flex>
+      </GridItem>
+
+      <GridItem pl="2" area={"chat"}>
+        {messages.length > 0 && <ContainerMessages messages={messages} />}
+      </GridItem>
+
+      <GridItem pl="2" area={"inputUser"}>
+        <InputUser
           input={input}
           handleInputChange={handleInputChange}
           handleSubmit={handleSubmit}
         />
-      </Box>
-    </Box>
+      </GridItem>
+    </Grid>
   );
 }
