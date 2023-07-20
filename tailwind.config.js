15c69febb28e6e20bfb9d03e9a7ab8d09b571f14
carlosqdev <carlosqdev@gmail.com>
2023-07-20 15:15:42 -0600

    add: theme tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      animation: {
+        typing: "blink 1s steps(5, start) infinite",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        gradient: "linear-gradient(180deg,rgba(53,55,64,0),#353740 58.85%)",
+      },
+      keyframes: {
+        blink: {
+          to: { visibility: "hidden" },
+        },
+      },
+      colors: {
+        gptlogo: "#10a37f",
+        gptdarkgray: "#202123",
+        gptgray: "#343541",
+        gptlightgray: "#444654",
       },
     },
   },
   plugins: [],
-}
+};
