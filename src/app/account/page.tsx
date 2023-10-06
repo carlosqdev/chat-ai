e89bf74586a968ccd54582aa7f48b15f908daaf5
carlosqdev <carlosqdev@gmail.com>
2023-10-05 21:09:34 -0600

    add auth with client component

---
+++ src/app/account/page.tsx
@@ -0,0 +1,14 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+// import { Database } from '../database.types'
+import AccountForm from "../../components/account-form";
+
+export default async function Account() {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return <AccountForm session={session} />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import Chat from "@/components/chat/chat";
-
-export const runtime = "edge";
+import AuthForm from "../components/auth-form";
 
 export default function Home() {
   return (
-    <main className="bg-gptgray">
-      <Chat />
+    <main className="min-h-screen grid place-content-center">
+      <div className="w-[90%] m-auto grid gap-5 lg:w-[320px]">
+        <h1 className="text-3xl font-bold text-center leading-10">Log In</h1>
+        <p className="text-center font-normal text-lg">
+          Log in with your account and start using our services.
+        </p>
+      </div>
+      <div className="col-6 auth-widget">
+        <AuthForm />
+      </div>
     </main>
   );
 }
