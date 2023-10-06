4f43a45c525009e19719ab53aa5554dbc3deb5fd
carlosqdev <carlosqdev@gmail.com>
2023-10-06 10:38:52 -0600

    add new next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    COHERE_API_KEY: process.env.COHERE_API_KEY,
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
   },
 };
 
